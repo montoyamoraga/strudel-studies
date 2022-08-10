84e0383c49abaed1cdf7e6700aa5b8f5f68c1530
aarón montoya-moraga <montoyamoraga@gmail.com>
2022-08-10 09:53:19 -0400

    add notes

--- src/modules/strudeldrums.js
+++ src/modules/strudeldrums.js
@@ -12,10 +12,10 @@ import { mini } from '@strudel.cycles/mini';
 class StrudelDrums {
   constructor() {
     this.kick = new Tone.MembraneSynth().chain(vol(0.8), out());
-    this.snare = new Tone.NoiseSynth().chain(vol(0.8), out());
+    // this.snare = new Tone.NoiseSynth().chain(vol(0.8), out());
     this.drums = stack(
-      mini('[c1*2]').tone(this.kick).bypass('<0@7 1>/8'),
-      mini('~ <x!7 [x@3 x]>').tone(this.snare).bypass('<0@7 1>/4'),
+      mini('[c1 c1]').tone(this.kick).bypass('<0@7 1>/8'),
+      // mini('~ <x!7 [x@3 x]>').tone(this.snare).bypass('<0@7 1>/4'),
     );
   }
   init() {
@@ -24,3 +24,15 @@ class StrudelDrums {
 }
 
 export { StrudelDrums };
+
+// const kick = new MembraneSynth().chain(vol(0.8), out());
+// const snare = new NoiseSynth().chain(vol(0.8), out());
+// const hihat = new MetalSynth()
+//   .set(adsr(0, 0.08, 0, 0.1))
+//   .chain(vol(0.3), out());
+// const drums = stack(
+//   '[c1 c1]'.tone(kick),
+//   // "~ <x!7 [x@3 x]>".tone(snare).bypass("<0@7 1>/4"),
+//   // "[~ c4]*2".tone(hihat)
+// );
+// stack(drums);
--- src/sketch.js
+++ src/sketch.js
@@ -1,17 +1,65 @@
-import { UserInterface } from './modules/userinterface.js';
-// import { StrudelDrums } from './modules/strudeldrums.js';
+// references
+// https://github.com/tidalcycles/strudel/tree/main/packages/tone
+// https://strudel.tidalcycles.org/tutorial/
 
-import { Tone } from '@strudel.cycles/tone';
+// import { UserInterface } from './modules/userinterface.js';
+// import { Tone } from '@strudel.cycles/tone';
 
 // variables for user interface
-let userInterface;
-let strudelDrums;
+// let userInterface;
+
+import {
+  sequence,
+  stack,
+  State,
+  TimeSpan,
+} from '@strudel.cycles/core';
+
+import {
+  Tone,
+  polysynth,
+  osc,
+  out,
+  membrane,
+} from '@strudel.cycles/tone';
+import { MembraneSynth } from 'tone';
+
+const pattern = sequence('c3', ['eb3', stack('g3', 'bb3')]).tone(
+  polysynth().set(osc('sawtooth4')).chain(out()),
+);
+
+const drums = sequence('c2', 'c2').tone(membrane().chain(out()));
+
+document
+  .getElementById('buttonStartStop')
+  .addEventListener('click', async () => {
+    await Tone.start();
+    Tone.getTransport().stop();
+
+    // const events = pattern
+    //   .query(new State(new TimeSpan(0, 4)))
+    //   .filter((e) => e.whole.begin.equals(e.part.begin));
+
+    // const events = drums
+    // .query(new State(new TimeSpan(0, 4)))
+    //   .filter((e) => e.whole.begin.equals(e.part.begin));
+
+    const events = drums
+      .query(new State(new TimeSpan(0, 1)))
+      .filter((e) => e.whole.begin.equals(e.part.begin));
+
+    events.forEach((event) =>
+      Tone.getTransport().schedule(
+        (time) => event.context.onTrigger(time, event),
+        event.whole.begin.valueOf(),
+      ),
+    );
+    Tone.getTransport().start('+0.1');
+  });
 
 function init() {
-  userInterface = new UserInterface(true);
-  userInterface.init();
-  // strudelDrums = new StrudelDrums();
-  // strudelDrums.init();
+  // userInterface = new UserInterface(true);
+  // userInterface.init();
 }
 
 init();
